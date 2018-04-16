8232120c807b3ba465f1b36e96226de343996e7d
Konstantin <zukaaman@gmail.com>
2018-04-16 23:08:13 +0400

    second

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+/*
+ * Custom
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  //= partials/performer-questions.js
+});
---
+++ src/js/partials/performer-questions.js
@@ -0,0 +1,90 @@
+var questionItem0 = document.querySelector(".questions-item0"),
+    questionItem1 = document.querySelector(".questions-item1"),
+    questionItem2 = document.querySelector(".questions-item2"),
+    questionItem3 = document.querySelector(".questions-item3"),
+    questionItem4 = document.querySelector(".questions-item4"),
+    questionItem5 = document.querySelector(".questions-item5"),
+    questionItem6 = document.querySelector(".questions-item6"),
+    questionItem7 = document.querySelector(".questions-item7"),
+    questionItem8 = document.querySelector(".questions-item8"),
+    questionItem9 = document.querySelector(".questions-item9"),
+    questionItem10 = document.querySelector(".questions-item10"),
+    questionItem11 = document.querySelector(".questions-item11"),
+    questionItem12 = document.querySelector(".questions-item12"),
+    questionItem13 = document.querySelector(".questions-item13"),
+    questionItem14 = document.querySelector(".questions-item14"),
+    questionItem15 = document.querySelector(".questions-item15"),
+    questionItem16 = document.querySelector(".questions-item16"),
+    questionItem17 = document.querySelector(".questions-item17");
+
+questionItem0.addEventListener("click", function() {
+  questionItem0.classList.toggle("closed");
+});
+
+questionItem1.addEventListener("click", function() {
+  questionItem1.classList.toggle("closed");
+});
+
+questionItem2.addEventListener("click", function() {
+  questionItem2.classList.toggle("closed");
+});
+
+questionItem3.addEventListener("click", function() {
+  questionItem3.classList.toggle("closed");
+});
+
+questionItem4.addEventListener("click", function() {
+  questionItem4.classList.toggle("closed");
+});
+
+questionItem5.addEventListener("click", function() {
+  questionItem5.classList.toggle("closed");
+});
+
+questionItem6.addEventListener("click", function() {
+  questionItem6.classList.toggle("closed");
+});
+
+questionItem7.addEventListener("click", function() {
+  questionItem7.classList.toggle("closed");
+});
+
+questionItem8.addEventListener("click", function() {
+  questionItem8.classList.toggle("closed");
+});
+
+questionItem9.addEventListener("click", function() {
+  questionItem9.classList.toggle("closed");
+});
+
+questionItem10.addEventListener("click", function() {
+  questionItem10.classList.toggle("closed");
+});
+
+questionItem11.addEventListener("click", function() {
+  questionItem11.classList.toggle("closed");
+});
+
+questionItem12.addEventListener("click", function() {
+  questionItem12.classList.toggle("closed");
+});
+
+questionItem13.addEventListener("click", function() {
+  questionItem13.classList.toggle("closed");
+});
+
+questionItem14.addEventListener("click", function() {
+  questionItem14.classList.toggle("closed");
+});
+
+questionItem15.addEventListener("click", function() {
+  questionItem15.classList.toggle("closed");
+});
+
+questionItem16.addEventListener("click", function() {
+  questionItem16.classList.toggle("closed");
+});
+
+questionItem17.addEventListener("click", function() {
+  questionItem17.classList.toggle("closed");
+});
